22960823978c484f005cee68c860355375127a6d
Shubhangi Mishra <shubhangimishra215@gmail.com>
2026-06-06 20:59:50 +0530

    Fix dependencies

--- client/React/src/utils/axios.js
+++ client/React/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: 'https://eventora-xa0v.onrender.com',
-    headers: {  // ← was 'header'
+    headers: {  
         'Content-Type': 'application/json',
     },
 });
