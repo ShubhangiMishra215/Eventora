8b838bc750b9d3a767a5febd1a15eb3562edd68f
Shubhangi Mishra <shubhangimishra215@gmail.com>
2026-06-06 21:03:23 +0530

    Changes made

--- client/React/src/utils/axios.js
+++ client/React/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://eventora-xa0v.onrender.com',
+    baseURL: 'https://eventora-xa0v.onrender.com/api',
     headers: {  
         'Content-Type': 'application/json',
     },
